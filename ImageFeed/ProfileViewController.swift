54e00142782b2557f516077884b761f65ee8b45c
smo_ozyyy <smoozydearth@yandex.ru>
2026-06-08 17:01:29 +0700

    Сверстан экран профиля

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+final class ProfileViewController {
+    //MARK: Outlets
+    
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var loginNameLabel: UILabel!
+    @IBOutlet var exitButton: UIButton!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    @IBAction func didTapExitButton() {
+        
+    }
+}
