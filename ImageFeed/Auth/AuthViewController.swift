9b6a305633522017e2441d7e63199bc9e1fc200e
smo_ozyyy <smoozydearth@yandex.ru>
2026-07-17 14:53:30 +0700

    -Замена методов ProgressHUD, на ранее созданный класс UIBlockingProgressHUD

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -51,9 +51,9 @@ final class AuthViewController: UIViewController {
 extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
         vc.dismiss(animated: true)
-        ProgressHUD.animate()
+        UIBlockingProgressHUD.show()
         oauth2Service.fetchAuthToken(code: code) { [weak self] result in
-            ProgressHUD.dismiss()
+            UIBlockingProgressHUD.dismiss()
             guard let self = self else { return }
             switch result{
             case .success(let token):
