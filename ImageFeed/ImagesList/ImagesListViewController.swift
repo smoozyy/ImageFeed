87ea123ce5151d48f6e8100c893a940f8d5dc055
smo_ozyyy <smoozydearth@yandex.ru>
2026-08-12 14:57:47 +0700

    -Добавлен градиент

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -106,11 +106,13 @@ extension ImagesListViewController {
         let photo = photos[indexPath.row]
         
         guard let url = URL(string: photo.thumbImageURL) else { return }
-        cell.cellImageView.kf.indicatorType = .activity
+        cell.showSkeleton()
         cell.cellImageView.kf.setImage(
             with: url,
             placeholder: UIImage(resource: .stub)
-        )
+        ) { [weak cell] result in
+            cell?.removeSkeleton()
+        }
         
         cell.dateLabel.text = dateFormatter.string(from: photo.createdAt ?? Date())
         let likeImage = UIImage(named: "like_button_\(photo.isLiked ? "on" : "off")")
--- ImageFeed/ProfileViewController/ProfileViewController.swift
+++ ImageFeed/ProfileViewController/ProfileViewController.swift
@@ -11,6 +11,7 @@ final class ProfileViewController: UIViewController {
     private var descriptionLabel: UILabel?
     private let profileService = ProfileService.shared
     private var profileLogoutService = ProfileLogoutService.shared
+    private var animationLayers = Set<CALayer>()
     
     //MARK: viewDidLoad
     
@@ -35,7 +36,7 @@ final class ProfileViewController: UIViewController {
             target: self,
             action: #selector(didTapButton)
         )
-        view.backgroundColor = .ypBlackIOS
+        
         exitButton.tintColor = .red
         nameLabel.textColor = .ypWhiteIOS
         profileLabel.textColor = .ypGrayIOS
@@ -48,6 +49,8 @@ final class ProfileViewController: UIViewController {
         descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
         exitButton.translatesAutoresizingMaskIntoConstraints = false
         avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+        avatarImageView.layer.cornerRadius = 35
+        avatarImageView.clipsToBounds = true
         view.addSubview(exitButton)
         view.addSubview(avatarImageView)
         view.addSubview(nameLabel)
@@ -80,11 +83,13 @@ final class ProfileViewController: UIViewController {
             descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
             
         ])
-        
+                
         self.avatarImageView = avatarImageView
         self.nameLabel = nameLabel
         self.profileLabel = profileLabel
         self.descriptionLabel = descriptionLabel
+        
+        showSkeleton()
         
         if let profile = profileService.profile {
             updateProfileDetails(profile: profile)
@@ -98,6 +103,59 @@ final class ProfileViewController: UIViewController {
     
     
     //MARK: Private Methods
+    
+    private func createGradientLayer(frame: CGRect, cornerRadius: CGFloat) -> CALayer {
+        let gradient = CAGradientLayer()
+        gradient.frame = frame
+        gradient.locations = [0.0, 0.1, 0.3]
+        gradient.colors = [
+            UIColor(red: 0.682, green: 0.686, blue: 0.706, alpha: 1).cgColor,
+            UIColor(red: 0.531, green: 0.533, blue: 0.553, alpha: 1).cgColor,
+            UIColor(red: 0.431, green: 0.433, blue: 0.453, alpha: 1).cgColor
+        ]
+        gradient.startPoint = CGPoint(x:0, y: 0.5)
+        gradient.endPoint = CGPoint(x: 1, y: 0.5)
+        gradient.cornerRadius = cornerRadius
+        gradient.masksToBounds = true
+        
+        let gradientChangeAnimation = CABasicAnimation(keyPath: "locations")
+        gradientChangeAnimation.duration = 1.0
+        gradientChangeAnimation.repeatCount = .infinity
+        gradientChangeAnimation.fromValue = [0.0, 0.1, 0.3]
+        gradientChangeAnimation.toValue = [0, 0.8, 1]
+        gradient.add(gradientChangeAnimation, forKey: "locationsChange")
+        return gradient
+    }
+    
+    private func showSkeleton() {
+        view.layoutIfNeeded()
+        guard
+            let avatarImageView = avatarImageView,
+            let nameLabel = nameLabel,
+            let profileLabel = profileLabel,
+            let descriptionLabel = descriptionLabel
+        else {return}
+        let avatarGradient = createGradientLayer(frame: avatarImageView.bounds, cornerRadius: 35)
+        animationLayers.insert(avatarGradient)
+        avatarImageView.layer.addSublayer(avatarGradient)
+        
+        let nameLabelGradient = createGradientLayer(frame: CGRect(origin: .zero, size: CGSize(width: 200 , height: 30 )), cornerRadius: 9 )
+        animationLayers.insert(nameLabelGradient)
+        nameLabel.layer.addSublayer(nameLabelGradient)
+        
+        let profileLabelGradient = createGradientLayer(frame: CGRect(origin: .zero, size: CGSize(width: 100, height: 20)), cornerRadius: 9)
+        animationLayers.insert(profileLabelGradient)
+        profileLabel.layer.addSublayer(profileLabelGradient)
+        
+        let descriptionLabelGradient = createGradientLayer(frame: CGRect(origin: .zero, size: CGSize(width: 150, height: 22)), cornerRadius: 9)
+        animationLayers.insert(descriptionLabelGradient)
+        descriptionLabel.layer.addSublayer(descriptionLabelGradient)
+    }
+    
+    private func removeSkeleton() {
+        animationLayers.forEach { $0.removeFromSuperlayer() }
+        animationLayers.removeAll()
+    }
     
     private func updateProfileDetails(profile: Profile) {
         nameLabel?.text = profile.name.isEmpty
@@ -121,12 +179,11 @@ final class ProfileViewController: UIViewController {
         
         let placeholderImage = UIImage(systemName: "person.circle.fill")?
             .withTintColor(.lightGray, renderingMode: .alwaysOriginal)
-            .withConfiguration(UIImage.SymbolConfiguration(pointSize: 70, weight: .regular, scale: .large))
+            .withConfiguration(UIImage.SymbolConfiguration(pointSize: 70, weight: .regular, scale: .medium))
         
         
         
         let proccessor = RoundCornerImageProcessor(cornerRadius: 35)
-        avatarImageView?.kf.indicatorType = .activity
         avatarImageView?.kf.setImage(
             with: url,
             placeholder: placeholderImage,
@@ -136,6 +193,7 @@ final class ProfileViewController: UIViewController {
                 .cacheOriginalImage, /// кэшируем изображение
                 .forceRefresh /// игнорируем кэш, чтобы обновить
             ]) { result in
+                self.removeSkeleton()
                 switch result {
                 case .success(let value):
                     print(value.image)
